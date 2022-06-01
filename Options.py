ab5485a78f8d8daee138e032d912216f68971066
svensglinz <svenglinz@bluewin.ch>
2022-06-01 18:13:12 +0200

    Initial Upload

--- Options.py
+++ Options.py
@@ -1,26 +1,29 @@
 import numpy as np
 import math as m
 from scipy import stats
+import pandas as pd
 
 """
 DESCRIPTION
 """
 
-class Vanilla_Option:
+class Option:
 
-    def __init__(self, type, S,K ,r,t, v = None, act_price = None, European = True):
+    def __init__(self, type, S,K ,r,t, div = 0, v = None, European = True):
         self.type = 1 if type == "Call" else -1
         self.K = K
         self.S = S
         self.v = v
         self.r = r
         self.t = t
-        self.act_price = act_price
         self.European = European
+        self.div = div
 
-    def price(self, method = "BSM", nstep = None, nsim = None):
+    def price(self, method = "BSM", nstep = None, nsim = None, path = False, antithetic = False, seed = None,
+              discrete = False):
 
         if method == "BSM":
+
             d1 = (np.log(self.S/self.K) + (self.r + 0.5 * self.v**2)*self.t)/(self.v * np.sqrt(self.t))
             d2 = d1 - self.v * np.sqrt(self.t)
 
@@ -38,7 +41,8 @@ class Vanilla_Option:
             dt = self.t / nstep
             u = m.exp(self.v * m.sqrt(dt))
             d = 1 / u
-            q = (m.exp(self.r * dt) - d) / (u - d)
+            q = (m.exp((self.r- self.div)*dt)-d)/(u-d)
+
             Tree = np.zeros((nstep + 1, nstep + 1))
             for j in range(nstep + 1):
                 for i in range(j + 1):
@@ -51,8 +55,11 @@ class Vanilla_Option:
                         if (j == nstep + 1):
                             option[i, j - 1] = max((Tree[i, j - 1] - self.K) * self.type, 0)
                         else:
-                            option[i, j - 1] = m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j])
-                return option[0, 0]
+                            if discrete = False:
+                                option[i, j - 1] = m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j])
+                            else:
+                                option[i, j - 1] = (1/m.pow((1+self.r), self.t/nstep)) * (q * option[i, j] + (1 - q) * option[i + 1, j])
+                price = option[0,0]
 
             if not self.European:
 
@@ -62,18 +69,27 @@ class Vanilla_Option:
                         if (j == nstep + 1):
                             option[i, j - 1] = max((Tree[i, j - 1] - self.K) * self.type, 0)
                         else:
-                            option[i, j - 1] = max(self.K - Tree[i, j - 1], m.exp(-self.r * dt) * (q * option[i, j] + \
-                                                                                                   (1 - q) * option[
-                                                                                                       i + 1, j]))
-                return option[0, 0]
+                            option[i, j - 1] = max((Tree[i, j - 1]-self.K)*self.type,
+                                                   m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j]))
+                price = option[0, 0]
 
+            if path:
+                return {"price":price, "tree": Tree}
+            else:
+                return price
 
         elif method == "MC":
 
+            np.random.seed(seed)
             #simulate stock price paths
             e = np.random.normal(0, 1, size=(nsim, nstep))
+            if antithetic:
+                e = np.concatenate((e, -e), axis = 0)
+            else:
+                pass
             drift = (self.r - 0.5 * self.v ** 2) * (self.t / nstep)
             shock = self.v * m.sqrt(self.t / nstep) * e
+
             S_t = self.S * np.exp((drift + shock).cumsum(axis=1))
 
 
@@ -81,65 +97,69 @@ class Vanilla_Option:
                 S_T = S_t[:, nstep - 1]
                 C_T = np.fmax((S_T - self.K) * self.type, 0)
                 price = np.exp(-self.r * self.t) * np.mean(C_T)
-                return price
+
+                if path:
+                    return {"price":price, "tree": pd.DataFrame(S_t)}
+                else:
+                    return price
 
             if not self.European:
-                pass
+                print("For Monte Carlo, only pricing for european Option is available")
+                return None
 
+    """
+    Theta: Time Decay per Day (1 year =365 Days) 
+    """
     def greeks(self, *args):
 
-        print("Greeks determined with BSM Model")
         greeks = {}
         d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.v ** 2) * self.t) / (self.v * np.sqrt(self.t))
         d2 = d1 - self.v * np.sqrt(self.t)
 
-        if self.type == "Call":
+        if self.type == 1:
             greeks["delta"] = stats.norm.cdf(d1)
-        elif self.type == "Put":
+        elif self.type == -1:
             greeks["delta"] = -stats.norm.cdf(-d1)
 
-        greeks["vega"] = vega = self.S * stats.norm.pdf(d1,0.0,1.0) * np.sqrt(self.t)
-        greeks["theta"] = - self.v / (2* m.sqrt(self.t)) * self.S * stats.norm.pdf(d1) - \
-                self.r * self.K * m.exp(-self.r * self.t) * stats.norm.cdf(d2)
-        greeks["rho"] = self.t* m.exp(-self.r * self.t) * self.K * stats.norm.cdf(d2)
-        greeks["gamma"] = self.S * stats.norm.pdf(d1) * m.sqrt(self.t)
+        greeks["vega"] = (self.S * stats.norm.pdf(d1) * np.sqrt(self.t)) / 100
+        greeks["theta"] = (- self.v / (2* m.sqrt(self.t)) * self.S * stats.norm.pdf(d1) - \
+                self.r * self.K * m.exp(-self.r * self.t) * stats.norm.cdf(d2))/365
+        greeks["rho"] = (self.t * np.exp(-self.r * self.t) * self.K * stats.norm.cdf(d2))/100
+        greeks["gamma"] = stats.norm.pdf(d1)/(self.v*self.S*np.sqrt(self.t))
 
-        return greeks.get(args[0])
+        if len(args) == 0:
+            return greeks
+        else:
+            return greeks.get(args[0])
 
-    def implied_vol(self):
+    def implied_vol(self, price):
 
         print("Implied Volatility determined with BSM Model")
+
         max_iter = 100
         precision = 1.0e-10
         v_guess = 0.5
 
         for i in range(0, max_iter):
             self.v = v_guess
-            vega = self.greeks("vega")
-            diff = self.price() - self.act_price
+            vega = self.greeks("vega") * 100
+            diff = self.price() - price
             if (abs(diff) < precision):
                 return v_guess
             v_guess = v_guess - diff / vega
 
 
-"""
-DESCRIPTION
-"""
+def CRR_Tree(u,d,r,t, nstep, S, div):
+    dt = t/nstep
+    Tree = np.zeros((nstep + 1, nstep + 1))
+    for j in range(nstep + 1):
+        for i in range(j + 1):
+            Tree[i, j] = S * m.pow(d, i) * m.pow(u, j - i)
+    return Tree
 
-class Exotic_Option:
-
-    def __init__(self, type, S,K ,r,t, v = None, act_price = None):
-        self.type = 1 if type == "Call" else -1
-        self.K = K
-        self.S = S
-        self.v = v
-        self.r = r
-        self.t = t
-        self.act_price = act_price
+def FRA():
 
 
 
-"""
-DESCRIPTION
-"""
+
 
