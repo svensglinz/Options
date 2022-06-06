10be6f90967c887885b4ffefb1e5c62773a4345c
svensglinz <svenglinz@bluewin.ch>
2022-06-06 18:36:05 +0200

    Initial Upload

--- Options.py
+++ Options.py
@@ -7,9 +7,10 @@ import pandas as pd
 DESCRIPTION
 """
 
+
 class Option:
 
-    def __init__(self, type, S,K ,r,t, div = 0, v = None, European = True):
+    def __init__(self, type, S, K, r, t, div=0, v=None, European=True):
         self.type = 1 if type == "Call" else -1
         self.K = K
         self.S = S
@@ -19,21 +20,21 @@ class Option:
         self.European = European
         self.div = div
 
-    def price(self, method = "BSM", nstep = None, nsim = None, path = False, antithetic = False, seed = None,
-              discrete = False):
+    def price(self, method="BSM", nstep=None, nsim=None, path=False, antithetic=False, seed=None):
 
         if method == "BSM":
 
-            d1 = (np.log(self.S/self.K) + (self.r + 0.5 * self.v**2)*self.t)/(self.v * np.sqrt(self.t))
+            d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.v ** 2) * self.t) / (self.v * np.sqrt(self.t))
             d2 = d1 - self.v * np.sqrt(self.t)
 
-            if self.European:
-                price = (self.S * stats.norm.cdf(d1* self.type) - self.K * \
-                        np.exp(-self.r * self.t) * stats.norm.cdf(d2 * self.type)) * self.type
-                return price
+            if (not self.European) or (self.div != 0):
+                print("BSM Method can only price European Options without dividends. Please change specifications!")
+                return None
 
-            if not self.European:
-                print("BSM Method can only price European Options. Please change specifications!")
+            if self.European and self.div == 0:
+                price = (self.S * stats.norm.cdf(d1 * self.type) - self.K *
+                         np.exp(-self.r * self.t) * stats.norm.cdf(d2 * self.type)) * self.type
+                return price
 
         elif method == "CRR":
 
@@ -41,7 +42,7 @@ class Option:
             dt = self.t / nstep
             u = m.exp(self.v * m.sqrt(dt))
             d = 1 / u
-            q = (m.exp((self.r- self.div)*dt)-d)/(u-d)
+            q = (m.exp((self.r - self.div) * dt) - d) / (u - d)
 
             Tree = np.zeros((nstep + 1, nstep + 1))
             for j in range(nstep + 1):
@@ -52,39 +53,38 @@ class Option:
                 option = np.zeros((nstep + 1, nstep + 1))
                 for j in range(nstep + 1, 0, -1):
                     for i in range(j):
-                        if (j == nstep + 1):
+                        if j == nstep + 1:
                             option[i, j - 1] = max((Tree[i, j - 1] - self.K) * self.type, 0)
                         else:
-                            if discrete = False:
-                                option[i, j - 1] = m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j])
-                            else:
-                                option[i, j - 1] = (1/m.pow((1+self.r), self.t/nstep)) * (q * option[i, j] + (1 - q) * option[i + 1, j])
-                price = option[0,0]
+                            option[i, j - 1] = m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j])
+
+                price = option[0, 0]
 
             if not self.European:
 
                 option = np.zeros((nstep + 1, nstep + 1))
                 for j in range(nstep + 1, 0, -1):
                     for i in range(j):
-                        if (j == nstep + 1):
+                        if j == nstep + 1:
                             option[i, j - 1] = max((Tree[i, j - 1] - self.K) * self.type, 0)
                         else:
-                            option[i, j - 1] = max((Tree[i, j - 1]-self.K)*self.type,
-                                                   m.exp(-self.r * dt) * (q * option[i, j] + (1 - q) * option[i + 1, j]))
+                            option[i, j - 1] = max((Tree[i, j - 1] - self.K) * self.type,
+                                                   m.exp(-self.r * dt) * (
+                                                           q * option[i, j] + (1 - q) * option[i + 1, j]))
                 price = option[0, 0]
 
             if path:
-                return {"price":price, "tree": Tree}
+                return {"price": price, "tree": Tree}
             else:
                 return price
 
         elif method == "MC":
 
             np.random.seed(seed)
-            #simulate stock price paths
+            # simulate stock price paths
             e = np.random.normal(0, 1, size=(nsim, nstep))
             if antithetic:
-                e = np.concatenate((e, -e), axis = 0)
+                e = np.concatenate((e, -e), axis=0)
             else:
                 pass
             drift = (self.r - 0.5 * self.v ** 2) * (self.t / nstep)
@@ -92,24 +92,24 @@ class Option:
 
             S_t = self.S * np.exp((drift + shock).cumsum(axis=1))
 
+            if (not self.European) or (self.div != 0):
+                print("MC Method can only price European Options without dividends. Please change specifications!")
+                return None
 
-            if self.European:
+            if self.European and self.div == 0:
                 S_T = S_t[:, nstep - 1]
                 C_T = np.fmax((S_T - self.K) * self.type, 0)
                 price = np.exp(-self.r * self.t) * np.mean(C_T)
 
                 if path:
-                    return {"price":price, "tree": pd.DataFrame(S_t)}
+                    return {"price": price, "tree": pd.DataFrame(S_t)}
                 else:
                     return price
-
-            if not self.European:
-                print("For Monte Carlo, only pricing for european Option is available")
-                return None
 
     """
     Theta: Time Decay per Day (1 year =365 Days) 
     """
+
     def greeks(self, *args):
 
         greeks = {}
@@ -118,14 +118,19 @@ class Option:
 
         if self.type == 1:
             greeks["delta"] = stats.norm.cdf(d1)
+            greeks["rho"] = (self.t * np.exp(-self.r * self.t) * self.K * stats.norm.cdf(d2)) / 100
+            greeks["theta"] = (- self.v / (2 * m.sqrt(self.t)) * self.S * stats.norm.pdf(d1) -
+                               self.r * self.K * m.exp(-self.r * self.t) * stats.norm.cdf(d2)) / 365
         elif self.type == -1:
             greeks["delta"] = -stats.norm.cdf(-d1)
+            greeks["rho"] = (self.t * np.exp(-self.r * self.t) * self.K * stats.norm.cdf(d2) -
+                             (self.t * self.K * m.exp(-self.r * self.t))) / 100
+            greeks["theta"] = (- self.v / (2 * m.sqrt(self.t)) * self.S * stats.norm.pdf(d1) -
+                               self.r * self.K * m.exp(-self.r * self.t) * stats.norm.cdf(d2) +
+                               self.r * self.K * m.exp(-self.r * self.t))/ 365
 
         greeks["vega"] = (self.S * stats.norm.pdf(d1) * np.sqrt(self.t)) / 100
-        greeks["theta"] = (- self.v / (2* m.sqrt(self.t)) * self.S * stats.norm.pdf(d1) - \
-                self.r * self.K * m.exp(-self.r * self.t) * stats.norm.cdf(d2))/365
-        greeks["rho"] = (self.t * np.exp(-self.r * self.t) * self.K * stats.norm.cdf(d2))/100
-        greeks["gamma"] = stats.norm.pdf(d1)/(self.v*self.S*np.sqrt(self.t))
+        greeks["gamma"] = stats.norm.pdf(d1) / (self.v * self.S * np.sqrt(self.t))
 
         if len(args) == 0:
             return greeks
@@ -144,7 +149,7 @@ class Option:
             self.v = v_guess
             vega = self.greeks("vega") * 100
             diff = self.price() - price
-            if (abs(diff) < precision):
+            if abs(diff) < precision:
                 return v_guess
             v_guess = v_guess - diff / vega
 
